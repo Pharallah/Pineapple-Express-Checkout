81bdb6cfbd26187c9c5b6d8d753a482a2c95e81e
Harry San Juan <harrycsanjuan@gmail.com>
2024-11-08 16:01:39 -0500

    upgraded db to have columns

--- server/seed.py
+++ server/seed.py
@@ -12,14 +12,36 @@ from models import db, Customer, Order, OrderItem, Category, Item
 
 customers = [
     {
-        'username': 'harry',
-        'email': 'harry@gmail.com',
-        ''
+        'username': 'Pharallah',
+        'email': 'pharallah@gmail.com',
+        'password': 'asflkjasf123'
+    },
+    {
+        'username': 'theERhusie',
+        'email': 'thaerhusie@gmail.com',
+        'password': 'asjldghads9973'
+
+    },
+    {
+        'username': 'PepperoniViceroy',
+        'email': 'pepperoniviceroy@gmail.com',
+        'password': 'nbouteb082752'
     }
 ]
 
 def create_customers():
-    pass
+    seeded_customers = []
+
+    for customer in customers:
+        new_customer = Customer(
+            username=customer.username,
+            email=customer.email
+        )
+        new_customer.password_hash = customer.password
+        seeded_customers.append(new_customer)
+
+    db.session.add_all(seeded_customers)
+    db.session.commit()    
 
 def create_orders():
     pass
@@ -39,32 +61,32 @@ if __name__ == '__main__':
         print("Starting seed...")
         # Seed code goes here!
         Customer.query.delete()
-        Order.query.delete()
-        OrderItem.delete()
-        Category.query.delete()
-        Item.query.delete()
+        # Order.query.delete()
+        # OrderItem.delete()
+        # Category.query.delete()
+        # Item.query.delete()
 
         print("Seeding Customers...")
         customers = create_customers()
         db.session.add_all(customers)
         db.session.commit()
 
-        print("Seeding Orders...")
-        orders = create_orders()
-        db.session.add_all(orders)
-        db.session.commit()
+        # print("Seeding Orders...")
+        # orders = create_orders()
+        # db.session.add_all(orders)
+        # db.session.commit()
 
-        print("Seeding OrderItems...")
-        order_items = create_order_items()
-        db.session.add_all(order_items)
-        db.session.commit()
+        # print("Seeding OrderItems...")
+        # order_items = create_order_items()
+        # db.session.add_all(order_items)
+        # db.session.commit()
 
-        print("Seeding Categories...")
-        categories = create_categories()
-        db.session.add_all(categories)
-        db.session.commit()
+        # print("Seeding Categories...")
+        # categories = create_categories()
+        # db.session.add_all(categories)
+        # db.session.commit()
 
-        print("Seeding Items...")
-        items = create_items()
-        db.session.add_all(items)
-        db.session.commit()
+        # print("Seeding Items...")
+        # items = create_items()
+        # db.session.add_all(items)
+        # db.session.commit()
