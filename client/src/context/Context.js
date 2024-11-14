e232d046ed0fb8b7b2274935b32537c38fbaf85c
Harry San Juan <harrycsanjuan@gmail.com>
2024-11-14 00:55:18 -0500

    added GET all state to Context

---
+++ client/src/context/Context.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { createContext } from 'react'
+import { useState, useEffect } from 'react'
+
+const Context = createContext()
+
+function ContextProvider({ children }) {
+    const [customers, setCustomers] = useState([])
+    const [orders, setOrders] = useState([])
+    const [orderItems, setOrderItems] = useState([])
+    const [categories, setCategories] = useState([])
+    const [items, setItems] = useState([])
+    
+    useEffect(() => {
+        fetch('/customers')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(customers => setCustomers(customers))
+    }, [])
+
+    useEffect(() => {
+        fetch('/orders')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(orders => setOrders(orders))
+    }, [])
+
+    useEffect(() => {
+        fetch('/orderitems')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(orderItems => setOrderItems(orderItems))
+    }, [])
+
+    useEffect(() => {
+        fetch('/categories')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(categories => setCategories(categories))
+    }, [])
+    
+    useEffect(() => {
+        fetch('/items')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(items => setItems(items))
+    }, [])
+  
+    return <Context.Provider value={
+        {
+            customers, setCustomers,
+            orders, setOrders,
+            orderItems, setOrderItems,
+            categories, setCategories,
+            items, setItems
+        }
+    }>{children}</Context.Provider>
+}
+
+export { Context, ContextProvider }
