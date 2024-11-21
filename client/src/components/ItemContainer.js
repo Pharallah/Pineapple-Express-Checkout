55ec67fc0e7f8c72ecca89c636f025d040399b44
Harry San Juan <harrycsanjuan@gmail.com>
2024-11-21 17:25:16 -0500

    added orderType toggle to ItemContainer

--- client/src/components/ItemContainer.js
+++ client/src/components/ItemContainer.js
@@ -1,16 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../context/Context';
+import { Switch } from '@headlessui/react';
 
 function ItemContainer() {
     const { currentUser, items, categories } = useContext(Context);
+    const [orderType, setOrderType] = useState("Catering");
+
+    console.log(orderType)
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(" ");
+    }
+
+    const handleToggle = () => {
+        setOrderType((prev) => (prev === "Catering" ? "Take-Out" : "Catering"));
+    };
+
+    // When a user clicks on "Add Item", the following will occur:
+    // Query the DB for the given user's Orders w/ order_status of "Pending Checkout"
+    // If not, create an Order --> Popup of an order form
+    // This needs: customer_id, order_type (Catering/Take-Out)
 
     return (
         <div className="bg-white">
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            {/* Order Type Toggle */}
+            <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="flex items-center justify-between mt-10 mb-1">
+                    <label className="relative inline-flex cursor-pointer items-center">
+                        <input
+                            type="checkbox"
+                            checked={orderType === "Take-Out"}
+                            onChange={handleToggle}
+                            className="peer sr-only"
+                        />
+                        <div className="peer relative flex h-8 w-42 items-center rounded-full bg-orange-200 px-2 after:absolute after:left-1 after:h-6 after:w-20 after:rounded-full after:bg-black/20 after:transition-all after:content-[''] peer-checked:after:translate-x-20 peer-focus:outline-none text-sm text-black">
+                            <span className="absolute left-3.4 text-md font-bold">Catering</span>
+                            <span className="absolute right-3 text-md font-bold">Take-Out</span>
+                        </div>
+                    </label>
+                </div>
+            </div>
+
+            {/* Products Section */}
+            <div className="max-w-2xl mx-auto py-2 px-4 sm:py-2 sm:px-6 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Products</h2>
 
                 {categories.map((category) => (
-                    <div key={category.id} className="mb-8">
+                    <div key={category.id} className="mb-4">
                         {/* Category Name */}
                         <h3 className="text-lg font-semibold text-gray-900 mb-4">
                             {category.name}
@@ -36,7 +72,9 @@ function ItemContainer() {
                                             {/* Item Name */}
                                             <div>
                                                 <h3 className="text-sm text-gray-700">{item.name}</h3>
-                                                <p className="mt-1 text-lg font-medium text-gray-900">${item.price}</p>
+                                                <p className="mt-1 text-lg font-medium text-gray-900">
+                                                    ${item.price}
+                                                </p>
                                             </div>
                                             {/* Add Item Button */}
                                             <button
